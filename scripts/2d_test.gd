85a2260d4a4149fd5de73c4c14d71e77250239eb
Timothy Cope <kirbycope@gmail.com>
2025-06-10 01:18:55 -0700

    controls

--- scripts/2d_test.gd
+++ scripts/2d_test.gd
@@ -3,14 +3,12 @@ extends Node2D
 @onready var text_edit: TextEdit = $Control/TextEdit
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
+## Set the current input type.
 func _input(event: InputEvent) -> void:
 	# Only process events that could trigger actions
-	if event is InputEventKey or event is InputEventJoypadButton or event is InputEventJoypadMotion or event is InputEventMouseButton:
+	if event is InputEventKey \
+	or event is InputEventJoypadButton \
+	or event is InputEventMouseButton:
 		var all_actions = InputMap.get_actions()
 		for action in all_actions:
 			if event.is_action_pressed(action):
